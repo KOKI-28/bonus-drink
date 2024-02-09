91cd1b2d706d4c55d8a9e2f89853c0f142182c58
koki <koki.okamoto1128@gmail.com>
2024-02-09 23:19:51 +0900

    ANSWER

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -11,11 +11,10 @@ class BonusDrink
 
     end
 
-    p amount.to_s + "本購入した場合、全部で" + total.to_s + "本飲める。"
+    p total
   end
 
   # 最初に100本購入した場合、トータルで何本飲めるか
   self.total_count_for(100)
-  
 
 end
