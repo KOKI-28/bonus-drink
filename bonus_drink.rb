05d13c36b4a1cd8d509792875c545dd02d39828d
koki <koki.okamoto1128@gmail.com>
2024-02-09 21:48:31 +0900

    my_answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,20 @@
 class BonusDrink
+
+  # 最初に購入した飲み物の本数から、トータルで飲める本数を算出するプログラム
   def self.total_count_for(amount)
-    0
+    num = amount
+    total = amount
+
+    while num >= 3
+      num -= 2
+      total += 1
+
+    end
+
+    p amount.to_s + "本購入した場合、全部で" + total.to_s + "本飲める。"
   end
+
+  # 最初に100本購入した場合、トータルで何本飲めるか
+  self.total_count_for(100)
+  
 end
